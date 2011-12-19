b5d31c77533a7e98ac8ea4259f5834d3c3e15377
soywiz <soywiz@gmail.com>
2011-12-20 00:15:29 +0100

    - Fixed make_eboot.bat

--- pspautotests/tests/make_eboot.bat
+++ pspautotests/tests/make_eboot.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
-del EBOOT.PBP %1.elf 2> NUL
+del "%1\..\EBOOT.PBP" "%1.elf" 2> NUL
 CALL %~dp0\make.bat %*
-IF NOT EXIST %1.elf GOTO end
-mksfo TESTMODULE param.sfo
-pack-pbp EBOOT.PBP param.sfo NUL NUL NUL NUL NUL %1.elf NUL > NUL
+IF NOT EXIST "%1.elf" GOTO end
+mksfo TESTMODULE "%1\..\param.sfo"
+pack-pbp "%1\..\EBOOT.PBP" "%1\..\param.sfo" NUL NUL NUL NUL NUL "%1.elf" NUL > NUL
 REM del %1.elf param.sfo
-del param.sfo
+del "%1\..\param.sfo"
 :end
