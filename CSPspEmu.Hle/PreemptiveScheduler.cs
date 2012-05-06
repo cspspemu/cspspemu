136c1687d8caa4966970567c8a4bd32d0d52112b
soywiz <soywiz@gmail.com>
2012-05-06 03:50:48 +0200

    - WIP Issue #43 - Added new PreemptiveScheduler that will be used by the new thread scheduler.

---
+++ CSPspEmu.Hle/PreemptiveScheduler.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSPspEmu.Hle
+{
+	public interface IPreemptiveItem
+	{
+		int Priority { get; }
+		bool Ready { get; }
+
+		//event Action<IPreemptiveItem> Updated;
+	}
+
+	public class PreemptiveScheduler<T> where T : IPreemptiveItem
+	{
+		LinkedList<T> CurrentReadyQueue = new LinkedList<T>();
+		List<T> Items = new List<T>();
+
+		bool NewItemsFirst;
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="NewItemsFirst"></param>
+		public PreemptiveScheduler(bool NewItemsFirst)
+		{
+			this.NewItemsFirst = NewItemsFirst;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="PreemptiveItem"></param>
+		public void Remove(T PreemptiveItem)
+		{
+			Items.Remove(PreemptiveItem);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="Item"></param>
+		public void Update(T Item)
+		{
+			if (!Items.Contains(Item))
+			{
+				//Items.Remove(PreemptiveItem);
+				Items.Add(Item);
+			}
+
+			if (Item.Ready && Item.Priority >= CurrentHighestPriority)
+			{
+				if (Item.Priority == CurrentHighestPriority)
+				{
+					AddItemToCurrentReadyQueue(Item);
+				}
+				else
+				{
+					ReScheduleHighestPriority();
+				}
+			}
+		}
+
+		private void AddItemToCurrentReadyQueue(T Item)
+		{
+			if (NewItemsFirst)
+			{
+				CurrentReadyQueue.AddFirst(Item);
+			}
+			else
+			{
+				CurrentReadyQueue.AddLast(Item);
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public int CurrentHighestPriority { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public T Current { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <remarks>If an item changes its priority, this method should be executed.</remarks>
+		public void ReScheduleHighestPriority()
+		{
+			if (Items.Count == 0) throw (new Exception("No items to schedule"));
+
+			CurrentHighestPriority = Items.Where(Item => Item.Ready).Max(Item => Item.Priority);
+
+			foreach (var Item in Items.Where(Item => Item.Ready && Item.Priority == CurrentHighestPriority))
+			{
+				AddItemToCurrentReadyQueue(Item);
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public void Next()
+		{
+			bool Found = false;
+			Current = default(T);
+
+			while (!Found)
+			{
+				// Queue is empty, lets find the higher Ready priority.
+				if (CurrentReadyQueue.Count == 0)
+				{
+					ReScheduleHighestPriority();
+				}
+
+				// Get next in the queue.
+				while (CurrentReadyQueue.Count > 0)
+				{
+					var Item = CurrentReadyQueue.RemoveFirstAndGet();
+					if (Item.Ready && Item.Priority == CurrentHighestPriority)
+					{
+						Current = Item;
+						Found = true;
+						CurrentReadyQueue.AddLast(Item);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ CSPspEmu.Tests/Hle/PreemptiveSchedulerTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSPspEmu.Hle;
+
+namespace CSPspEmu.Tests.Hle
+{
+	[TestClass]
+	public class PreemptiveSchedulerTest
+	{
+		public class PreemptiveItem : IPreemptiveItem
+		{
+			public string Name;
+			public int Priority { get; set; }
+			public bool Ready { get; set; }
+
+			public override string ToString()
+			{
+				return String.Format("PreemptiveItem(Name='{0}',Priority={1}, Ready={2})", Name, Priority, Ready);
+			}
+		}
+
+		PreemptiveScheduler<PreemptiveItem> Scheduler = new PreemptiveScheduler<PreemptiveItem>(NewItemsFirst : false);
+
+		PreemptiveItem PreemptiveItem1 = new PreemptiveItem() { Name = "1", Priority = 10, Ready = true };
+		PreemptiveItem PreemptiveItem2 = new PreemptiveItem() { Name = "2", Priority = 10, Ready = true };
+		PreemptiveItem PreemptiveItem3 = new PreemptiveItem() { Name = "3", Priority = 10, Ready = false };
+
+		PreemptiveItem PreemptiveItem4 = new PreemptiveItem() { Name = "4", Priority = 8, Ready = true };
+
+		[TestMethod]
+		[ExpectedException(typeof(Exception))]
+		public void TestNextWithoutItems()
+		{
+			Scheduler.Next();
+		}
+
+		[TestMethod]
+		public void TestNextSingleItem()
+		{
+			Scheduler.Update(PreemptiveItem1);
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem1, Scheduler.Current);
+			}
+		}
+
+		[TestMethod]
+		public void TestNextTwoItemsWithSamePriority()
+		{
+			Scheduler.Update(PreemptiveItem1);
+			Scheduler.Update(PreemptiveItem2);
+			Scheduler.Update(PreemptiveItem3);
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem1, Scheduler.Current);
+
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem2, Scheduler.Current);
+			}
+		}
+
+		[TestMethod]
+		public void TestNextTwoItemsWithSamePriorityAndOtherWithLowerPriority()
+		{
+			Scheduler.Update(PreemptiveItem1);
+			Scheduler.Update(PreemptiveItem2);
+			Scheduler.Update(PreemptiveItem3);
+			Scheduler.Update(PreemptiveItem4);
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem1, Scheduler.Current);
+
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem2, Scheduler.Current);
+			}
+
+			PreemptiveItem1.Ready = false;
+			//Scheduler.Put(PreemptiveItem1);
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem2, Scheduler.Current);
+			}
+
+			PreemptiveItem2.Ready = false;
+			//Scheduler.Put(PreemptiveItem2);
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem4, Scheduler.Current);
+			}
+
+			PreemptiveItem4.Ready = false;
+			Scheduler.Update(PreemptiveItem4);
+
+			PreemptiveItem1.Ready = true;
+			Scheduler.Update(PreemptiveItem1);
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem1, Scheduler.Current);
+			}
+
+			PreemptiveItem4.Priority = 11;
+			PreemptiveItem4.Ready = true;
+			Scheduler.Update(PreemptiveItem4);
+			//Scheduler.ReScheduleHighestPriority();
+
+			for (int n = 0; n < 3; n++)
+			{
+				Scheduler.Next();
+				Assert.AreEqual(PreemptiveItem4, Scheduler.Current);
+			}
+		}
+	}
+}
