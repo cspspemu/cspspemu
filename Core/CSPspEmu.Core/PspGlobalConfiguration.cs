9e390641bfd75460d7182f8088b8c3170dbe05d0
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-07-16 20:48:06 +0200

    Do not require embeded txt files. Instead just update class file

--- Core/CSPspEmu.Core/PspGlobalConfiguration.cs
+++ Core/CSPspEmu.Core/PspGlobalConfiguration.cs
@@ -5,56 +5,8 @@ namespace CSPspEmu.Core
 {
     public static class PspGlobalConfiguration
     {
-        private static string _CurrentVersion;
-        private static int? _CurrentVersionNumeric;
-
-        public static string CurrentVersion
-        {
-            get
-            {
-                if (_CurrentVersion == null)
-                {
-                    _CurrentVersion = Assembly.GetEntryAssembly()
-                        .GetManifestResourceStream("CSPspEmu.References.version_current.txt").ReadAllContentsAsString();
-                }
-                return _CurrentVersion;
-            }
-        }
-
-        public static int CurrentVersionNumeric
-        {
-            get
-            {
-                try
-                {
-                    if (!_CurrentVersionNumeric.HasValue)
-                    {
-                        _CurrentVersionNumeric = int.Parse(Assembly.GetEntryAssembly()
-                            .GetManifestResourceStream("CSPspEmu.References.version_current_numeric.txt")
-                            .ReadAllContentsAsString());
-                    }
-                    return _CurrentVersionNumeric.Value;
-                }
-                catch
-                {
-                    return 0;
-                }
-            }
-        }
-
-        private static string _GitRevision;
-
-        public static string GitRevision
-        {
-            get
-            {
-                if (_GitRevision == null)
-                {
-                    _GitRevision = Assembly.GetEntryAssembly()
-                        .GetManifestResourceStream("CSPspEmu.References.git_revision.txt").ReadAllContentsAsString();
-                }
-                return _GitRevision;
-            }
-        }
+        public static string CurrentVersion = "20130728";
+        public static int CurrentVersionNumeric = 0;
+        public static string GitRevision = "git-unknown";
     }
 }
