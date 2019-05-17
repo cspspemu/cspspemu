a80676c9630845f35ef0841618b0eacc6cad432c
soywiz <soywiz@gmail.com>
2019-05-17 17:31:54 +0200

    Improve Rasterizer to have bounds into account

--- CSPspEmu/GPU/Impl/Soft/GpuImplSoft.cs
+++ CSPspEmu/GPU/Impl/Soft/GpuImplSoft.cs
@@ -185,7 +185,7 @@ namespace CSPspEmu.Core.Gpu.Impl.Soft
         {
             var triangle = new Triangle(p0, p1, p2);
             Rasterizer.Rasterizer.RasterizeTriangle(triangle.P0.P, triangle.P1.P, triangle.P2.P, triangle,
-                DrawTriangleFastTest, 0, 272);
+                DrawTriangleFastTest, 0, 271, 0, 511);
         }
 
         //static public Vector4 LerpRatios(Vector4 a, Vector4 b, Vector4 c, float ra, float rb, float rc) => (a * ra) + (b * rb) * (c * rc);
@@ -212,8 +212,8 @@ namespace CSPspEmu.Core.Gpu.Impl.Soft
                 }
                 else
                 {
-                    var colorA = LerpRatios(C0, C1, C2, a.Ratio0, a.Ratio1, a.Ratio2);
-                    var colorB = LerpRatios(C0, C1, C2, b.Ratio0, b.Ratio1, b.Ratio2);
+                    var colorA = LerpRatios(C0, C1, C2, a.Ratios.X, a.Ratios.Y, a.Ratios.Z);
+                    var colorB = LerpRatios(C0, C1, C2, b.Ratios.X, b.Ratios.Y, b.Ratios.Z);
                     //Console.WriteLine($"{a.Ratio0}, {a.Ratio1}, {a.Ratio2}");
                     DrawPixelsFast(y, a.X, b.X, colorA, colorB);
                 }
@@ -222,46 +222,31 @@ namespace CSPspEmu.Core.Gpu.Impl.Soft
 
         public static void Swap<T>(ref T lhs, ref T rhs)
         {
-            T temp = lhs;
+            var temp = lhs;
             lhs = rhs;
             rhs = temp;
         }
 
         private void DrawSprite(VPoint a, VPoint b)
         {
-            var minX = Math.Min(a.P.X, b.P.X).Clamp(0, 512);
-            var maxX = Math.Max(a.P.X, b.P.X).Clamp(0, 512);
-            var minY = Math.Min(a.P.Y, b.P.Y).Clamp(0, 272);
-            var maxY = Math.Max(a.P.Y, b.P.Y).Clamp(0, 272);
+            var minX = Math.Min(a.P.X, b.P.X).Clamp(0, 511);
+            var maxX = Math.Max(a.P.X, b.P.X).Clamp(0, 511);
+            var minY = Math.Min(a.P.Y, b.P.Y).Clamp(0, 271);
+            var maxY = Math.Max(a.P.Y, b.P.Y).Clamp(0, 271);
             //Console.WriteLine($"Sprite {a}, {b}");
-            for (var y = minY; y <= maxY; y++)
-            {
-                DrawPixelsFast(y, minX, maxX, a.IColor);
-            }
+            for (var y = minY; y <= maxY; y++) DrawPixelsFast(y, minX, maxX, a.IColor);
         }
 
         private void DrawPixelsFast(int y, int x0, int x1, uint color)
         {
-            if (y < 0 || y > 272) return;
-            var startX = x0.Clamp(0, 512);
-            var endX = x1.Clamp(0, 512);
-            var ptr = (uint*) Memory.PspAddressToPointerSafe((uint) (DrawAddress + (y * 512 + startX) * 4));
-            var count = endX - startX;
-            for (var n = 0; n < count; n++) ptr[n] = color;
+            var ptr = (uint*) Memory.PspAddressToPointerSafe((uint) (DrawAddress + (y * 512) * 4));
+            for (var n = x0; n < x1; n++) ptr[n] = color;
         }
 
         private void DrawPixelsFast(int y, int x0, int x1, Vector4 c1, Vector4 c2)
         {
-            if (y < 0 || y >= 272) return;
-            var startX = x0.Clamp(0, 512);
-            var endX = x1.Clamp(0, 512);
-            var ptr = (uint*) Memory.PspAddressToPointerSafe((uint) (DrawAddress + (y * 512 + startX) * 4));
-            var count = endX - startX;
-            for (int n = 0; n < count; n++)
-            {
-                var ratio = (startX + n).RatioInRange(x0, x1);
-                ptr[n] = new RgbaFloat(Vector4.Lerp(c1, c2, ratio)).Int;
-            }
+            var ptr = (uint*) Memory.PspAddressToPointerSafe((uint) (DrawAddress + (y * 512) * 4));
+            for (var n = x0; n < x1; n++) ptr[n] = new RgbaFloat(Vector4.Lerp(c1, c2, n.RatioInRange(x0, x1))).Int;
         }
 
         public override void Finish(GpuStateStruct* gpuState)
--- CSPspEmu/Rasterizer/Rasterizer.cs
+++ CSPspEmu/Rasterizer/Rasterizer.cs
@@ -1,68 +1,84 @@
 using System;
 using System.Diagnostics;
+using System.Numerics;
 using System.Runtime.CompilerServices;
 using CSharpUtils.Extensions;
 using CSPspEmu.Utils;
 
 namespace CSPspEmu.Rasterizer
 {
-    public delegate void RasterizeDelegate<T>(int y, ref RasterizerResult left, ref RasterizerResult right, ref T context);
-        
+    public delegate void RasterizeDelegate<T>(int y, ref RasterizerResult left, ref RasterizerResult right,
+        ref T context);
+
     public class Rasterizer
     {
-        public static void RasterizeTriangle<T>(RasterizerPoint p0, RasterizerPoint p1, RasterizerPoint p2, T param,
-            RasterizeDelegate<T> rowHandler, int clampY0 = int.MinValue, int clampY1 = int.MaxValue)
+        public static void RasterizeTriangle<T>(
+            RasterizerPoint p0, RasterizerPoint p1, RasterizerPoint p2,
+            T param,
+            RasterizeDelegate<T> rowHandler,
+            int ymin = int.MinValue, int ymax = int.MaxValue, int xmin = int.MinValue, int xmax = int.MaxValue
+        )
         {
             // ReSharper disable InvocationIsSkipped
             Debug.Assert(p0.Y <= p1.Y);
             Debug.Assert(p1.Y <= p2.Y);
 
-            var y0 = p0.Y.Clamp(clampY0, clampY1);
+            var y0 = p0.Y.Clamp(ymin, ymax);
             var y1 = p1.Y;
-            var y2 = p2.Y.Clamp(clampY0, clampY1);
+            var y2 = p2.Y.Clamp(ymin, ymax);
 
-            RasterizerResult xa, xb;
-            
-            for (var y = y0; y <= y1; y++)
+            RasterizerResult r0, r1;
+
+            for (var y = y0; y <= y2; y++)
             {
-                InterpolateX(y, p0, p2, 1, out xa);
-                InterpolateX(y, p0, p1, 2, out xb);
-                if (xa.X > xb.X) Swap(ref xa, ref xb);
-                rowHandler(y, ref xa, ref xb, ref param);
-            }
-            
-            for (var y = y1 + 1; y <= y2; y++)
-            {
-                InterpolateX(y, p0, p2, 1, out xa);
-                InterpolateX(y, p1, p2, 0, out xb);
-                if (xa.X > xb.X) Swap(ref xa, ref xb);
-                rowHandler(y, ref xa, ref xb, ref param);
+                InterpolateX(y, p0, p2, 1, out r0);
+                if (y <= y1) InterpolateX(y, p0, p1, 2, out r1); else InterpolateX(y, p1, p2, 0, out r1);
+
+                if (r0.X > r1.X) Swap(ref r0, ref r1);
+
+                var rx0 = r0.X;
+                var rx1 = r1.X;
+
+                if (rx0 < xmin || rx0 > xmax || rx1 < xmin || rx1 > xmax)
+                {
+                    var x0 = rx0.Clamp(xmin, xmax);
+                    var x1 = rx1.Clamp(xmin, xmax);
+
+                    r0 = new RasterizerResult(x0, Vector3.Lerp(r0.Ratios, r1.Ratios, x0.RatioInRange(rx0, rx1)));
+                    r1 = new RasterizerResult(x1, Vector3.Lerp(r0.Ratios, r1.Ratios, x1.RatioInRange(rx0, rx1)));
+                }
+
+                rowHandler(y, ref r0, ref r1, ref param);
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void InterpolateX(int y, RasterizerPoint a, RasterizerPoint b, int notUsedIndex,
-            out RasterizerResult result)
+        private static void InterpolateX(
+            int y,
+            RasterizerPoint a, RasterizerPoint b,
+            int notUsedIndex,
+            out RasterizerResult result
+        )
         {
             var ratio = (y - a.Y) / (float) (b.Y - a.Y);
             var iratio = 1 - ratio;
-            var res = ratio.Interpolate(a.X, b.X);
+            var x = ratio.Interpolate(a.X, b.X);
             switch (notUsedIndex)
             {
                 case 0:
-                    result = new RasterizerResult(res, 0, iratio, ratio);
+                    result = new RasterizerResult(x, new Vector3(0, iratio, ratio));
                     return;
                 case 1:
-                    result = new RasterizerResult(res, iratio, 0, ratio);
+                    result = new RasterizerResult(x, new Vector3(iratio, 0, ratio));
                     return;
                 case 2:
-                    result = new RasterizerResult(res, iratio, ratio, 0);
+                    result = new RasterizerResult(x, new Vector3(iratio, ratio, 0));
                     return;
                 default:
                     throw new Exception();
             }
         }
-        
+
         private static void Swap<T>(ref T lhs, ref T rhs)
         {
             T temp = lhs;
@@ -80,16 +96,12 @@ namespace CSPspEmu.Rasterizer
     public struct RasterizerResult
     {
         public readonly int X;
-        public readonly float Ratio0;
-        public readonly float Ratio1;
-        public readonly float Ratio2;
+        public readonly Vector3 Ratios;
 
-        public RasterizerResult(int x, float ratio0, float ratio1, float ratio2)
+        public RasterizerResult(int x, Vector3 ratios)
         {
             X = x;
-            Ratio0 = ratio0;
-            Ratio1 = ratio1;
-            Ratio2 = ratio2;
+            Ratios = ratios;
         }
 
         public override string ToString() => this.ToStringDefault();
@@ -106,7 +118,7 @@ namespace CSPspEmu.Rasterizer
             X = x;
             Y = y;
         }
-        public override string ToString() => this.ToStringDefault();
 
+        public override string ToString() => this.ToStringDefault();
     }
 }
